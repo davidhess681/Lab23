eb5a824d03065457c1929ca923fd5ef711f631bc
david <david.hess681@gmail.com>
2018-11-13 22:04:34 -0500

    first commit for Lab23. Databases are neat

---
+++ Lab23/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab23.Models;
+
+namespace Lab23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+         
+        public ActionResult UserRegistration()
+        {
+            return View();
+        }
+        public ActionResult AddUser(User newUser)
+        {
+            CoffeeShopDBLab23Entities1 DB = new CoffeeShopDBLab23Entities1();
+            DB.Users.Add(newUser);
+            DB.SaveChanges();
+            ViewBag.AddedItem = "Item was successfully added";
+            return View("UserRegistration");
+        }
+    }
+}
---
+++ Lab23/Scripts/myScript.js
@@ -0,0 +1,107 @@
+﻿var matching = false;
+var check_match = function () {
+    if (document.getElementById("email").value === document.getElementById("cfem").value) {
+        document.getElementById("message").innerHTML = "matching!";
+        document.getElementById("message").style.color = "green";
+        matching = true;
+    }
+    else {
+        document.getElementById("message").innerHTML = "not matching!";
+        document.getElementById("message").style.color = "red";
+        matching = false;
+    }
+}
+
+var fnRegex = /^[A-Z]+[A-z]{1,30}$/
+var lnRegex = /^[A-Z]+[A-z]{1,30}$/
+var emRegex = /^([A-z0-9\.]{5,30})@([A-z]{5,10})\.([a-z]{2,3})$/
+var pwRegex = /^[A-z0-9]{8,30}$/
+var pnRegex = /^\d{3}-?\d{3}-?\d{4}$/
+
+var fnIsValid = false;
+var validFN = function () {
+    if (fnRegex.test(document.getElementById("fn").value)) {
+        document.getElementById("fnMessage").innerHTML = "Valid!";
+        document.getElementById("fnMessage").style.color = "green";
+        fnIsValid = true;
+    }
+    else {
+        document.getElementById("fnMessage").innerHTML = "Invalid!";
+        document.getElementById("fnMessage").style.color = "red";
+        fnIsValid = false;
+    }
+}
+
+var lnIsValid = false;
+var validLN = function () {
+    if (lnRegex.test(document.getElementById("ln").value)) {
+        document.getElementById("lnMessage").innerHTML = "Valid!";
+        document.getElementById("lnMessage").style.color = "green";
+        lnIsValid = true;
+    }
+    else {
+        document.getElementById("lnMessage").innerHTML = "Invalid!";
+        document.getElementById("lnMessage").style.color = "red";
+        lnIsValid = false;
+    }
+}
+
+var emIsValid = false;
+var validEM = function () {
+    if (document.getElementById("cfem").value !== "") {
+        check_match();
+    }
+    if (emRegex.test(document.getElementById("email").value)) {
+        document.getElementById("emMessage").innerHTML = "Valid!";
+        document.getElementById("emMessage").style.color = "green";
+        emIsValid = true;
+    }
+    else {
+        document.getElementById("emMessage").innerHTML = "Invalid!";
+        document.getElementById("emMessage").style.color = "red";
+        emIsValid = false;
+    }
+}
+
+var pwIsValid = false;
+var validPW = function () {
+    if (document.getElementById("cfpw").value !== "") {
+        pwMatch();
+    }
+    if (pwRegex.test(document.getElementById("pw").value)) {
+        console.log("true");
+        document.getElementById("pwMessage").innerHTML = "Valid!";
+        document.getElementById("pwMessage").style.color = "green";
+        pwIsValid = true;
+    }
+    else {
+        console.log("false");
+        document.getElementById("pwMessage").innerHTML = "Invalid!";
+        document.getElementById("pwMessage").style.color = "red";
+        pwIsValid = false;
+
+    }
+}
+
+var pwMatching = false;
+var pwMatch = function () {
+    if (document.getElementById("pw").value === document.getElementById("cfpw").value) {
+        document.getElementById("cfpwMessage").innerHTML = "matching!";
+        document.getElementById("cfpwMessage").style.color = "green";
+        pwMatching = true;
+    }
+    else {
+        document.getElementById("cfpwMessage").innerHTML = "not matching!";
+        document.getElementById("cfpwMessage").style.color = "red";
+        pwMatching = false;
+    }
+}
+
+function Validate() {
+    if (fnIsValid && lnIsValid && emIsValid && matching &&  pwIsValid && pwMatching) {
+        return true;
+    }
+    alert("Please make sure all fields are valid")
+    return false;
+
+}
